ee0da1ee91e0cccbd434d2e33e5bac37dbd0f00d
Kindmark <39938776+Kindmark@users.noreply.github.com>
2022-01-23 16:27:36 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("My Name is Dug");
+
+string Robot;
+Robot = "Robot Man";
+Console.WriteLine("Hi " + Robot);
+Console.WriteLine("What is your name?");
+//This is probably broken
+string Robot;
+Robot = Console.ReadLine();
+Console.WriteLine(Robot);
+
+Console.WriteLine("Hi, " + Robot);
+
+
